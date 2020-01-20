70e6495bcc2316610504c7fa885ee9916635989b
GaryTheStTrooper <GaryTheStTrooper@users.noreply.github.com>
2020-01-20 00:51:58 -0500

    basics
    -Dosctring on pages.py explaining basic game structure
    -Basic fields defined and documented on Player/Group Classes
    -Base templates created

--- models.py
+++ models.py
@@ -1,20 +1,27 @@
 from otree.api import (
-    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
-    Currency as c, currency_range
+    models, BaseConstants, BaseSubsession, BaseGroup, BasePlayer
 )
 
+import controls as ctrl
 
-author = 'Your name here'
+author = 'Marco Gutierrez and Skyler Stewart'
 
 doc = """
-Your app description
+Money and Politics App
 """
 
 
 class Constants(BaseConstants):
-    name_in_url = 'MoneyPolitics'
+    name_in_url = 'DecisionStudy'
     players_per_group = 9
     num_rounds = 1
+
+    # There are some parameters that may vary during the development of this app. In order to make this as soft coded as
+    # possible, the code should be flexible enough to allow changes in this ones and obtain them from an external
+    # .py/txt file (If you find a better way, feel free to update the code with it)
+    task_endowments = ctrl.task_endowments
+    number_of_messages = ctrl.number_of_messages
+    message_cost = ctrl.message_cost
 
 
 class Subsession(BaseSubsession):
@@ -22,8 +29,45 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+
+    # Amount of players who will receive a luck based income
+    lucky_players = models.IntegerField()
+
+    # Chosen Tax Policy Parameters
+    chosen_progressivity = models.FloatField(min=0)
+    chosen_tax_rate = models.FloatField(min=0)
+
+    # Amount collected after the tax policy parameter has been decided
+    tax_revenue = models.CurrencyField(min=0)
 
 
 class Player(BasePlayer):
-    pass
+
+    # Real Effort Earnings
+    real_effort_earnings = models.CurrencyField(min=0)
+
+    # Ranking on real effort task
+    ranking = models.IntegerField(min=1, max=9)
+
+    # Shuffled == True if player's income will be shuffled, False if not
+    shuffled = models.BooleanField()
+
+    # Earnings after the shuffling
+    base_earnings = models.CurrencyField(min=0)
+
+    # Message to be sent (It should only have 500 characters. This has been implemented on PreparingMessage.html)
+    message = models.LongStringField()
+    # Amount of receivers of player's message (players with 9 or 15)
+    message_receivers = models.IntegerField(min=0, max=Constants.players_per_group)
+    # Eligible receiver (player with 9 or 15 as endowment)
+    eligible_receiver = models.BooleanField()
+    # Actual receiver of at least one of the messages
+    actual_receiver = models.BooleanField()
+    # Number of messages received
+    messages_received = models.IntegerField(min=0)
+    # Total cost for sending the messages
+    total_messaging_costs = models.CurrencyField()
+
+    # Preferred Tax Policy Parameters
+    progressivity = models.FloatField(min=0)
+    tax_rate = models.FloatField(min=0)
--- pages.py
+++ pages.py
@@ -2,13 +2,79 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+"""
+The provisional structure for the pages of this app is going to be as follows:
 
-class MyPage(Page):
+1. Instructions
+
+2. A real effort task: 
+    Players will play a Tetris or another game in order to obtain some provisional income. This has to be one of the
+    values of the following vector (depending on their ranking in the task)
+    
+    [9, 15, 15, 15, 25, 25, 40, 80, 125]
+    
+3. Information about whether their incomes are going to be random or from the real effort task:
+    50% 3 people paid by luck; (1,2,3[a],4,5[b],6,7,8,9[c]) -> (1,2,c,4,a,6,7,8,b)
+    50% 6 people paid by luck; similar, but 6 are shuffled
+
+4. Preparing Communication: 
+    Players will have the ability to send a single message to one or many players with 10 or 15 as endowment. This will 
+    have a cost which should be edited on the models.py or on a txt imported into the models.py file to avoid 
+    hard coding
+
+5. Deciding which policy parameter should be chosen for voting
+    
+    They will be asked about which policy system they prefer: 
+    1. Taxes: They can vote which should be the base tax rate (let's say 20%) that the people with low income should 
+    pay, but not the proportion in which this tax rate will increase when people have a greater income (there could be 
+    a fixed increase in 2% on the tax rate per each additional point) 
+    
+    2. Progressivity. They can't vote which should be the base tax rate (there is a fixed 20% tax rate) that the people
+    with low income should pay, but they can choose the proportion in which this tax rate will increase when people have
+    a greater income (an increase in 2% on the tax rate per each additional point or 4% if they prefer) 
+
+6. Choosing the tax policy parameter: Depending on the chosen system, they will be able to say:
+
+    1. Taxes: Which is their preferred base tax rate
+    2. Progressivity: Which is their preferred increase on the base tax rate per additional point
+    
+    The median of the preferred parameters is going to be the one applied to everyone.
+
+7. Payoffs:
+
+"""
+
+
+class Instructions(Page):
+    group_by_arrival_time = True
+
+
+class RealEffort(Page):
+    pass
+
+
+class PreparingMessage(Page):
+    form_model = 'player'
+    form_fields = ['message']
+
+    def vars_for_template(self):
+        player = self.player
+        return {'message': player.message}
+
+
+class ReceivingMessage(Page):
+    pass
+
+
+class TaxSystem(Page):
+    pass
+
+
+class TaxParameter(Page):
     pass
 
 
 class ResultsWaitPage(WaitPage):
-
     def after_all_players_arrive(self):
         pass
 
@@ -18,7 +84,12 @@ class Results(Page):
 
 
 page_sequence = [
-    MyPage,
+    Instructions,
+    RealEffort,
+    PreparingMessage,
+    ReceivingMessage,
+    TaxSystem,
+    TaxParameter,
     ResultsWaitPage,
     Results
 ]
