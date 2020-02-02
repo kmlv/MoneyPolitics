001ffe0c2f35ab596c55765087a52d26f1c3ed77
GaryTheStTrooper <GaryTheStTrooper@users.noreply.github.com>
2020-02-02 10:21:39 -0500

    Update models.py

--- models.py
+++ models.py
@@ -210,7 +210,7 @@ class Player(BasePlayer):
     base_earnings = models.CurrencyField(min=0)
 
     # Message to be sent (It should only have 500 characters. This has been implemented on PreparingMessage.html)
-    message = models.CharField(max_length=500, label='Write the message you want to send (max. 500 characters)')
+    message = models.LongStringField(max_length=500, label='Write the message you want to send (max. 500 characters)')
     # Amount of receivers of player's message (players with 9 or 15)
     amount_message_receivers = models.IntegerField(min=0, max=Constants.players_per_group, label='Write your preferred '
                                                                                                  'number of message '
