ae8f67ffd700837ebfb949b44bab634c7cc84b66
Grecia Barandiaran <greciabarandiaran@gmail.com>
2022-12-03 13:05:59 -0500

    add ravens task

--- models.py
+++ models.py
@@ -30,6 +30,15 @@ class Constants(BaseConstants):
     instructions_template = "MoneyPolitics/Instructions.html"
     instructions_button = "MoneyPolitics/Instructions_Button.html"
 
+    #Ravens config:
+    config = {
+            'test_number': 2,
+            '1' : [2, 3, 3, 7, 5, 6, 4, 1, 7, 6] , 
+            '2' : [6, 1, 2, 1, 7, 3, 4, 6, 5, 2],
+            '3' : [3, 1, 3, 8, 2, 8, 8, 8, 5, 2], 
+            '4' : [4, 2, 5, 6, 4, 3, 4, 2, 1, 6],
+            'points': [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5]
+     }
     # Real Effort Tasks
     tetris = "MoneyPolitics/Tetris.html"
     diamonds = "MoneyPolitics/Diamonds.html"
@@ -77,6 +86,20 @@ class Subsession(BaseSubsession):
 
         elif self.session.config["matching"] == "random":
             self.group_randomly()
+        #Ravenstask
+        for player in self.get_players():
+            player.test_num = Constants.config['test_number']
+            player.Q1_ans = Constants.config[str(player.test_num)][0]
+            player.Q2_ans = Constants.config[str(player.test_num)][1]
+            player.Q3_ans = Constants.config[str(player.test_num)][2]
+            player.Q4_ans = Constants.config[str(player.test_num)][3]
+            player.Q5_ans = Constants.config[str(player.test_num)][4]
+            player.Q6_ans = Constants.config[str(player.test_num)][5]
+            player.Q7_ans = Constants.config[str(player.test_num)][6]
+            player.Q8_ans = Constants.config[str(player.test_num)][7]
+            player.Q9_ans = Constants.config[str(player.test_num)][8]
+            player.Q10_ans = Constants.config[str(player.test_num)][9]
+    
 
 
 class Group(BaseGroup):
@@ -461,6 +484,92 @@ class Player(BasePlayer):
     transcription_8_correct = models.StringField(default='')
     transcription_9_correct = models.StringField(default='')
     transcription_10_correct = models.StringField(default='')
+
+    #RavensTask:
+    rand_id = models.IntegerField(initial=0)
+    test_num = models.IntegerField(initial=0)
+    score = models.FloatField(initial=0)
+    ranking = models.IntegerField(initial=0)
+    Q1_ans = models.IntegerField(initial=0)
+    Q2_ans = models.IntegerField(initial=0)
+    Q3_ans = models.IntegerField(initial=0)
+    Q4_ans = models.IntegerField(initial=0)
+    Q5_ans = models.IntegerField(initial=0)
+    Q6_ans = models.IntegerField(initial=0)
+    Q7_ans = models.IntegerField(initial=0)
+    Q8_ans = models.IntegerField(initial=0)
+    Q9_ans = models.IntegerField(initial=0)
+    Q10_ans = models.IntegerField(initial=0)
+   
+    Question_1 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_2 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_3 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_4 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_5 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_6 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_7 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_8 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_9 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    Question_10 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+        label = '''
+            Your Choice 
+            '''
+                )
+    def get_score(self):
+        question_ans = [self.Q1_ans, self.Q2_ans, self.Q3_ans,self. Q4_ans, self.Q5_ans, self.Q6_ans, self.Q7_ans, self.Q8_ans, self.Q9_ans, self.Q10_ans]
+        questions = [self.Question_1, self.Question_2, self.Question_3, self.Question_4, self.Question_5, self.Question_6, self.Question_7, self.Question_8, self.Question_9, self.Question_10]
+        i = 0 
+        for x in question_ans:
+            if questions[i] == x:
+                self.score += Constants.config['points'][i]
+            i =i+1
+        return self.score
     
     # Real Effort Earnings
     real_effort_earnings = models.CurrencyField(min=0)
--- pages.py
+++ pages.py
@@ -19,6 +19,154 @@ def linear_payoff(endowment, slope, tax):
 class GroupingPage(WaitPage):
     group_by_arrival_time = True
 
+class Question_1(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_1.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_1']
+
+class Question_2(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_2.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_2']
+
+class Question_3(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_3.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_3']
+class Question_4(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_4.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_4']
+
+class Question_5(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_5.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_5']
+
+class Question_6(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_6.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_6']
+class Question_7(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_7.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_7']
+
+class Question_8(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_8.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_8']
+
+class Question_9(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_9.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_9']
+
+class Question_10(Page):
+	def get_timeout_seconds(self):
+		return 75
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.save()
+	def vars_for_template(self):
+		Q_url = '/static/MoneyPolitics/sample_{index}/sample_{index}/Q_10.PNG'.format(index = self.player.test_num)
+
+		return {
+			'Q': Q_url
+		  }
+	form_model = 'player'
+	form_fields = ['Question_10']    
+
 class SumGame(Page):
   
     form_model = 'player'
@@ -967,6 +1115,16 @@ class ResultsAfterBeliefs(Page):
 # There should be a waiting page after preparing the message and before receiving one
 page_sequence = [
     GroupingPage,
+    Question_1,
+    Question_2,
+    Question_3,
+    Question_4,
+    Question_5,
+    Question_6,
+    Question_7,
+    Question_8,
+    Question_9,
+    Question_10,
     TranscriptionTask,
     SumGame,
     SliderTask1,
